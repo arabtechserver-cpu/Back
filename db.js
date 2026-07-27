0404d81f45eb7795d80cee46b4dc5324e889ea5b
Mithaly Admin <admin@mithaly.com>
2026-07-27 22:03:00 +0200

    Add linked_categories column to database schema

--- db.js
+++ db.js
@@ -476,7 +476,8 @@ async function createTables() {
       fields TEXT DEFAULT '[]',
       fields_title VARCHAR(255) DEFAULT 'بيانات الخدمة',
       parent_id INT REFERENCES categories(id) ON DELETE SET NULL DEFAULT NULL,
-      show_in_menu BOOLEAN DEFAULT false
+      show_in_menu BOOLEAN DEFAULT false,
+      linked_categories TEXT DEFAULT '[]'
     );
     
     ALTER TABLE categories ADD COLUMN IF NOT EXISTS show_in_menu BOOLEAN DEFAULT true;
