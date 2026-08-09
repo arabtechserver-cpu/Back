454f5e0e381e06ca8b454b43a1e92f138c8afbae
Mithaly Admin <admin@mithaly.com>
2026-08-09 02:28:18 +0200

    Increase payload limit to 500mb for large backups

--- server.js
+++ server.js
@@ -111,8 +111,8 @@ app.use(cors(corsOptions));
 
 app.options('*', cors(corsOptions));
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
 // Enable gzip/brotli compression for all responses
 app.use(compression());
