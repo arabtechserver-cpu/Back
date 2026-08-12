88bae4aca4305a252cb3ea1e99c2ec6806f47324
arabtechserver-cpu <arabtechserver-cpu@github.com>
2026-08-12 02:34:55 -0700

    API auto-sync improvements and WebP image processor

--- test_db.js
+++
@@ -1,1 +0,0 @@
-const { allQuery } = require('./db.js'); (async () => { try { const res = await allQuery('SELECT * FROM api_providers'); console.log(res); } catch(e) { console.error(e); } process.exit(); })();
